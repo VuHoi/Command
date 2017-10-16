ae995e794a36c4a11cbd2e83e6894979a5aadb77
hoideptrai123 <toilahoi007@gmail.com>
2017-10-16 20:11:51 +0700

    change

--- app/src/main/java/vn/nks/sunny/phieucongtac/MainActivity.java
+++ app/src/main/java/vn/nks/sunny/phieucongtac/MainActivity.java
@@ -1,7 +1,9 @@
 package vn.nks.sunny.phieucongtac;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Handler handler = new Handler();
+        Runnable r = new Runnable() {
+            public void run() {
+                Toast.makeText(MainActivity.this, "haha", Toast.LENGTH_SHORT).show();
+            }
+        };
+        handler.postDelayed(r, 5000);
     }
 }
